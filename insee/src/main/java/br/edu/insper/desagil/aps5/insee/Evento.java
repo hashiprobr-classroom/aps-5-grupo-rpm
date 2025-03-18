a51c883ea47f39746b465f899144de94e3f5a339
phbrandi <pedrohab1@al.insper.edu.br>
2025-03-18 20:27:13 -0300

    criando classes ph

---
+++ insee/src/main/java/br/edu/insper/desagil/aps5/insee/Evento.java
@@ -0,0 +1,27 @@
+package br.edu.insper.desagil.aps5.insee;
+
+public class Evento {
+    private DataTempo inicio;
+    private DataTempo fim;
+
+    public Evento(DataTempo inicio, DataTempo fim) {
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public DataTempo getInicio() {
+        return inicio;
+    }
+    public DataTempo getFim() {
+        return fim;
+    }
+
+    public boolean ehAntes(){
+        if (inicio.obterValor()> fim.obterValor()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
