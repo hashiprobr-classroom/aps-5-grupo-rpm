a4f53411417d3af35f4ebe9ee439cd2483671830
phbrandi <pedrohab1@al.insper.edu.br>
2025-03-20 14:45:26 -0300

    Extração do método encontaMaior na classe Item

--- isail/src/main/java/br/edu/insper/desagil/aps5/isail/Item.java
+++ isail/src/main/java/br/edu/insper/desagil/aps5/isail/Item.java
@@ -23,18 +23,23 @@ public class Item {
         return nome;
     }
 
+    private int encontraMaior() {
+        int maior = 0;
+        for (int i = 1; i < ofertas.size(); i++) {
+            if (ofertas.get(maior) < ofertas.get(i)) {
+                maior = i;
+            }
+        }
+        return maior;
+    }
+
     public void fazLance(Comprador comprador, double oferta) {
         if (oferta < minimo) {
             return;
         }
 
         if (!ofertas.isEmpty()) {
-            int maior = 0;
-            for (int i = 1; i < ofertas.size(); i++) {
-                if (ofertas.get(maior) < ofertas.get(i)) {
-                    maior = i;
-                }
-            }
+            int maior = encontraMaior();
             if (oferta <= ofertas.get(maior)) {
                 return;
             }
@@ -51,13 +56,9 @@ public class Item {
         if (ofertas.isEmpty()) {
             System.out.println("Nenhum lance foi feito ainda!");
         } else {
-            int maior = 0;
-            for (int i = 1; i < ofertas.size(); i++) {
-                if (ofertas.get(maior) < ofertas.get(i)) {
-                    maior = i;
-                }
-            }
+            int maior = encontraMaior();
             System.out.println(compradores.get(maior) + " ofereceu " + ofertas.get(maior) + " (" + momentos.toString() + ")");
         }
     }
+
 }
